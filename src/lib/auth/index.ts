de4c189b8e3038caf2b48be0f6e7a2428fef93b6
Vaidii <165685848+vaidii2008@users.noreply.github.com>
2026-08-27 15:41:12 +0100

    chore: set trustHost for production auth (#10)

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -10,6 +10,7 @@ const credentialsSchema = z.object({
 });
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   session: { strategy: "jwt" },
   pages: {
     signIn: "/login",
